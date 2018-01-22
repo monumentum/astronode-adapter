9f78464599d2f50dc471f8fc9b84ce4855f18f11
Guilherme Diego <guidiego.expgames@gmail.com>
2018-01-22 15:53:10 -0200

    Add: unit test to modules

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.DataAdapter = require('./lib/adapter/data');
+exports.DataMethods = require('./lib/adapter/methods');
 exports.EngineAdapter = require('./lib/adapter/engine');
 
 exports.utils = require('./lib/util');
