2b17d172f0652e04569ff9a511fd495616964b7a
Guilherme Diego <guidiego.expgames@gmail.com>
2018-01-22 00:59:18 -0200

    Implement: CI/Deploy stuff

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.DataAdapter = require('./lib/adapter/data');
 exports.EngineAdapter = require('./lib/adapter/engine');
 
 exports.utils = require('./lib/util');
+exports.errors = require('./lib/error');
