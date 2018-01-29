4402247a977fad3044f263fd44a0001710f2a63f
Guilherme Diego <guidiego.expgames@gmail.com>
2018-01-29 09:55:49 -0200

    Fix: Engine to accept the new inputs from astronode

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 exports.DataAdapter = require('./lib/adapter/data');
 exports.DataMethods = require('./lib/adapter/methods');
 exports.EngineAdapter = require('./lib/adapter/engine');
-
-exports.utils = require('./lib/util');
-exports.errors = require('./lib/error');
