6cf94d133a015cd37476a215cb0d076fb7e85e72
Guilherme Diego <guidiego.expgames@gmail.com>
2018-02-08 23:34:57 -0200

    Implement: Auth Adapter

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.DataAdapter = require('./lib/adapter/data');
 exports.DataMethods = require('./lib/adapter/methods');
 exports.EngineAdapter = require('./lib/adapter/engine');
+exports.AuthAdapter = require('./lib/adapter/auth');
